245f2e2e0c18e7bca404a659dff460533995d02e
juunini <juuni.ni.i@gmail.com>
2020-09-27 19:15:58 +0900

    Add section `Projects`

--- src/App.tsx
+++ src/App.tsx
@@ -66,6 +66,45 @@ function App(): JSX.Element {
             </article>
           </div>
         </section>
+
+        <section id="Project">
+          <h2>프로젝트</h2>
+
+          <ul>
+            <li>
+              <div>
+                <img src="" alt="project thumbnail" />
+              </div>
+
+              <div>
+                <h3>프로젝트 명1</h3>
+                <p>프로젝트 설명1</p>
+              </div>
+            </li>
+
+            <li>
+              <div>
+                <img src="" alt="project thumbnail" />
+              </div>
+
+              <div>
+                <h3>프로젝트 명2</h3>
+                <p>프로젝트 설명2</p>
+              </div>
+            </li>
+
+            <li>
+              <div>
+                <img src="" alt="project thumbnail" />
+              </div>
+
+              <div>
+                <h3>프로젝트 명3</h3>
+                <p>프로젝트 설명3</p>
+              </div>
+            </li>
+          </ul>
+        </section>
       </main>
     </>
   );
