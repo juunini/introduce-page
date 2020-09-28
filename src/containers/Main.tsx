2bf5ec7dac55e862f5ca4b5bc15f0776c73b44b4
juunini <juuni.ni.i@gmail.com>
2020-09-28 11:58:36 +0900

    Make `Intro` And `Main` containers

---
+++ src/containers/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Intro from './Sections/Intro';
+
+function Main(): JSX.Element {
+  return (
+    <main>
+      <Intro />
+    </main>
+  );
+}
---
+++ src/containers/Sections/Intro.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Intro(): JSX.Element {
+  return (
+    <section id="Intro">
+      <div>
+        <h2>JUUNINI</h2>
+        <p>
+          Front-end &#38; Back-end
+          <br />
+          Developer
+        </p>
+      </div>
+      <div>
+        <img src="" alt="profile" />
+      </div>
+    </section>
+  );
+}
+
+export default Intro;
